26e6232943687c6758101959cd87f0ba6147a03c
Nicklas <nicklas.hug@gmail.com>
2020-10-07 11:29:42 +0200

    Get link, description and education type

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from bs4 import BeautifulSoup
 for i in range(151):
     r = requests.get(f"https://www.ug.dk/alfabetisk/uddannelser?page={i}")
     bs = BeautifulSoup(r.text, 'html.parser')
-    titles = bs.find_all("h2")
-    for edu in titles:
-        if edu.string is not None:
-            print(edu.string)
+    lst = bs.find_all("div", "views-row")
+    for edu in lst:
+        print(edu.find("h2").string)
+        print(edu.find("a")["href"])
+        edu_lst = edu.find_all("div", "field-item even")
+        print(edu_lst[0].string)
+        print(edu_lst[1].string)
